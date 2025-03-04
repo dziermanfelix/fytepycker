263648ff2343602ec52ffcae1e0cf2805e0bcc0d
dziermanfelix <dziermanfelix@gmail.com>
2025-03-04 13:54:24 +0000

    updating css for fighter

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -35,7 +35,7 @@ const Events = () => {
 
   const Fighter = ({ img, name, url }) => (
     <div className='flex-row items-center text-center justify-between'>
-      <img src={img} alt={name} className='w-50 h-50 object-contain' />
+      <img src={img} alt={name} className='w-50 h-50 object-contain mb-2' />
       <a href={url} target='_blank' rel='noopener noreferrer' className='underline font-semibold'>
         {name}
       </a>
