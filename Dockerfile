ed64f345c885179b72b47cc41904d6a1a1f53ade
dziermanfelix <dziermanfelix@gmail.com>
2025-04-18 23:25:44 -0500

    Fytepycker Deployment v0.0.34

--- Dockerfile
+++ Dockerfile
@@ -52,4 +52,4 @@ COPY entrypoint.sh /app/entrypoint.sh
 RUN chmod +x /app/entrypoint.sh
 
 ENTRYPOINT ["/app/entrypoint.sh"]
-CMD ["daphne", "-b", "0.0.0.0", "-p", "$PORT", "core.asgi:application"]
+CMD ["daphne"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # Only run migrate and daphne if no other command was passed
 if [[ "$1" == "daphne" ]]; then
   python manage.py migrate
-  exec "$@"
+  daphne -b 0.0.0.0 -p $PORT core.asgi:application
 else
   exec "$@"
 fi
