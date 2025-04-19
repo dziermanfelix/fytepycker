ebab0ad9b1a07131d43df69c86983b886d8532a0
dziermanfelix <dziermanfelix@gmail.com>
2025-04-19 16:33:11 -0500

    Fytepycker Deployment v0.0.59

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,20 +4,24 @@ set -e
 echo "[entrypoint] Running with args: $@"
 echo "[entrypoint] Script path: $0"
 echo "[entrypoint] Arg1: $1"
+echo "[entrypoint] Arg2: $2"
+echo "[entrypoint] Arg3: $3"
+echo "[entrypoint] Arg4: $4"
 echo "[entrypoint] All args: $@"
 echo "ls /app..."
 ls -la /app
 
-if [[ "$1" == "web" ]]; then
+if [[ "$4" == "web" ]]; then
   echo "[entrypoint] Starting web dyno..."
   python manage.py migrate
   playwright install chromium
   exec daphne -b 0.0.0.0 -p $PORT core.asgi:application
 
-elif [[ "$1" == "worker" ]]; then
+elif [[ "$4" == "worker" ]]; then
   echo "[entrypoint] Starting worker dyno..."
   exec celery -A core worker --loglevel=info
 
 else
+  echo "[entrypoint] Running command directly: $@"
   exec "$@"
 fi
