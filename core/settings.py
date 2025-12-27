a1b474aa7a17066a647e102cb1df57a35050e3cc
dziermanfelix <dziermanfelix@gmail.com>
2025-12-26 19:11:28 -0600

    cleaning up authentication issues

--- core/settings.py
+++ core/settings.py
@@ -185,8 +185,6 @@ AUTH_USER_MODEL = 'accounts.User'
 # REST Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
