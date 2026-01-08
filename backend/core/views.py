2ba20f47dab7d51737489ac0a707a05f18c4f423
dziermanfelix <dziermanfelix@gmail.com>
2026-01-08 08:28:53 -0600

    [deploy] Fytepycker Deployment v0.2.14

--- backend/core/views.py
+++ backend/core/views.py
@@ -6,7 +6,7 @@ import os
 class FrontendAppView(View):
     def get(self, request):
         try:
-            with open(os.path.join(os.path.dirname(__file__), '../frontend/dist/index.html')) as f:
+            with open(os.path.join(os.path.dirname(__file__), '../../frontend/dist/index.html')) as f:
                 return HttpResponse(f.read())
         except FileNotFoundError:
             return HttpResponse("index.html not found", status=501)
